ec9959da397580183b0d6154227972c9743ccbad
Mantra0804 <32033786+Mantra0804@users.noreply.github.com>
2020-07-08 22:27:03 +0530

    presentation & container

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import Menu from './components/MenuComponent';
-import logo from './logo.svg';
-import './App.css';
-import {Navbar , NavbarBrand} from 'reactstrap';
+import React ,{Component} from 'react';
+import Main from './components/MainComponent';
 
-function App() {
+class App  extends Component {
+
+render(){
   return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
+    <div>
+      <Main />
     </div>
     
   );
+ }
 }
 
 export default App;
